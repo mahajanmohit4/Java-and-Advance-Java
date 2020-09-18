90d154bb9ddf510e88f4306b491b3a2eea88d08d
Mohit <mahajanmohit4@gmail.com>
2020-09-17 20:56:55 -0700

    day2

---
+++ Assignment1/04.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String args[]){
+        byte no = 4;
+        int number = (byte)no;
+        System.out.println("no is :  " + number);
+    }
+}
---
+++ Practice/02_Ternary_Bitwise.java
@@ -0,0 +1,44 @@
+class Main{
+    public static void main(String args[]){
+        System.out.println("Hello Class ");
+       //Binary to Digit
+    //    int a = 0b1000;   
+    //    int b = 0B1000;
+    //    System.out.println(a);
+    //    System.out.println(b);
+    //    -------------------------
+
+// swapping two number
+    //    int i = 5; //101
+    //    int j = 4; //100
+    //    int a = i & j; //100
+    //    int b = i | j; //101
+    //    System.out.println(a);
+    //    System.out.println(b);
+
+
+    //    int a = 8; //1000
+    //    int b = a<<2; //1000-00 increment by 2 bits
+    //    System.out.println(b);
+
+        Object obj1;
+        Object obj2;
+
+        if(true)
+        {
+            obj1 = new Integer(10);
+        }
+        else
+            obj2 = new Double(5.5);
+
+        System.out.println(obj1);
+    //    obj2 == true ? new Integer(10):new Double(5.5);
+        // System.out.println(obj2);
+
+    //     int i=6;
+    //     int j=7;
+    //     j = i==1 ? 6 : 7;
+    //     System.out.println(j);
+        
+    }
+}
