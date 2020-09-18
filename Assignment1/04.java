74ddc258b1e124f9cf11c3fc6f74c77770142efd
Mohit <mahajanmohit4@gmail.com>
2020-09-17 23:04:46 -0700

    Day2

--- Assignment1/04.java
+++ Assignment1/04.java
@@ -1,7 +1,9 @@
 class Main{
     public static void main(String args[]){
-        byte no = 4;
-        int number = (byte)no;
+        byte no1 = 4;
+        byte no2 = 2;
+        byte sum = no1+no2
+        int number = (byte)sum;
         System.out.println("no is :  " + number);
     }
 }
