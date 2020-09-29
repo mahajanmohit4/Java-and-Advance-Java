3cdd48bc076e260366231f7db4694cbd038e2ba0
Mohit <mahajanmohit4@gmail.com>
2020-09-29 17:11:41 +0530

    a

--- demo/src/quizz/Pr1.java
+++ demo/src/quizz/Pr1.java
@@ -4,7 +4,9 @@ package quizz;
 
 public class Pr1 {
 	public static void main(String[] args) {
-		B obj = new B();
+		for(int i=0,j=5 ; i<=5 ;i++,j--)
+			System.out.println("i : "+i+" j : "+j);
 		System.out.println("Mohit");
+		
 	}
 }
