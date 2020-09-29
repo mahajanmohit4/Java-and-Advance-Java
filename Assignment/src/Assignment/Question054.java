a35b5bf54d6bdefcc775b44e99ef7aad4b810e57
Mohit <mahajanmohit4@gmail.com>
2020-09-29 16:31:00 +0530

    assignment

---
+++ Assignment/src/Assignment/Question054.java
@@ -0,0 +1,8 @@
+package Assignment;
+
+public class Question054 {
+	public static void main(String[] args) {
+		
+	}
+}
+
---
+++ Assignment/src/practice/pr03.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class pr03 {
+	public static void main(String[] args) {
+		System.out.println(Math.random());
+		System.out.println((int)(Math.random() * 101));
+	}
+}
